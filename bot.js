69d90976cc1e54a457f7e3004e717b16664db856
mbacookie <mbacookie42@googlemail.com>
2020-04-21 11:01:05 +0100

    Core code changes
    Removed broken prefix and broken code, stripped to basic

--- bot.js
+++ bot.js
@@ -6,40 +6,10 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message.mentions', msg => {
-  if (msg.content === '<@!135751892402700288') {
-    msg.reply('Dont ping cookie you shit https://tenor.com/view/ping-discord-everyone-gif-14518610');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'aboose Dictator') {
-    msg.reply('Always! <376271023403958273>');
-  }
-});
-
 client.on('message', msg => {
   if (msg.content === 'hug me') {
     msg.reply('*hugs you*');
   }
-});
-
-const prefix = "sb!";
-client.on ("message", (message) => {
-	
-	msg = message.content.toLowerCase();
-	
-	if (message.author.bot) return;
-	
-	mention = message.mentions.users.first();
-	
-	if (msg.startsWith (prefix + "send")) {
-		if (mention == null) { return; }
-		message.delete();
-		mentionMessage = message.content.slice (8);
-		mention.sendMessage (mentionMessage);
-		message.channel.send ("done!");
-	}
 });
 
 client.login(auth.token);
