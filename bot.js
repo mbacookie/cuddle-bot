7001feaa3e4e6b6e1aca79cfee331c4927645926
mbacookie <mbacookie42@googlemail.com>
2020-05-09 13:53:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ client.on("message", (message) => {
     message.channel.send("pong!");
   } else
   if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("```Current commands are the following:-
+    message.channel.send("```Current commands are the following
                          "Hug me"
                          "I need a hug"
                          "Cuddles"
