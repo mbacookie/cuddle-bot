db81ebe702b0c37cc92f9808ead73603fe5bd694
mbacookie <mbacookie42@googlemail.com>
2020-05-12 20:47:08 +0100

    prefix change, bot core changes
    Prefix changed from ">~< " to "¬ "
    Core bot changes mean certain commands require the prefix to now be used rather then using event listeners 
    See "readme" for what commands require a prefix

--- bot.js
+++ bot.js
@@ -7,54 +7,6 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'i need a cuddle') {
-    msg.reply('*cuddles you*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'cuddles') {
-    msg.reply('*gives you a cuddle*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'hug me') {
-    msg.reply('*hugs you*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'i need a hug') {
-    msg.reply('*pulls you close and hugs you*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'I need a hug') {
-    msg.reply('*pulls you close and hugs you*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'Hug me') {
-    msg.reply('*hugs you*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'Cuddles') {
-    msg.reply('*gives you a cuddle*');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'I need a cuddle') {
-    msg.reply('*cuddles you*');
-  }
-});
-
-client.on('message', msg => {
   if (msg.content === 'Cookie') {
     msg.reply('*gives you a cookie* :cookie:');
   }
@@ -90,7 +42,7 @@ client.on('message', msg => {
   }
 });
 
-const prefix = "\ ";
+const prefix = "¬ ";
 client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
@@ -99,7 +51,7 @@ client.on("message", (message) => {
     message.channel.send("pong!");
   } else
   if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("Current commands are the following- \n 	Hug me \n 	I need a hug \n 	Cuddles \n 	I need a cuddle \n 	Cookie \n \n commands do not need '>~<' placed in front to work");
+    message.channel.send("Current commands are the following- \n 	Hug me \n 	I need a hug \n 	Cuddles \n 	I need a cuddle \n 	Cookie");
   } else
   if (message.content.startsWith(prefix + "I need a cuddle")) {
 	  message.reply("*Cuddles you*");
